9c778c3563c9b920fa5d7eec9fb5f86d9532be09
807573515 <wanghangan@qq.com>
2017-09-10 14:54:08 +0800

    添加了第三方API 和错误处理 仍有BUG

--- src/js/app.js
+++ src/js/app.js
@@ -4,36 +4,64 @@ function init(){
 	$(document).ready(function(){
 //定义全局变量 map
 var map;
+// var search=ko.observable("");
+// var names=['天安门','故宫博物院','汉庭酒店','医院','王府井百货'];
+
 	var m={
 		initCenter:[116.397428, 39.90923],
 		panel_toggle:ko.observable(false),
-		jump:false,
 		panel:$('#panel'),
 		menu_btn:$('#menu_btn'),
 		list:$('#list li'),
-		header_title_width:$('#header_title').width()+40, 
+		header_title_width:$('#header_title').width()+40,
 		markers:[],
 		infowindows:[],
 		transform_width:ko.observable(0),
+		html:new Array(5),
+		search:ko.observable(""),
+		/*测试代码*/
+		fn:function(){
+			console.log(m);
+		},
+		/*测试代码*/
+		search:ko.observable(""),
+		names:['天安门','故宫博物院','汉庭酒店','医院','王府井百货'],
+		filteredList:ko.computed(function(){
+			
+			// console.log(m);
+			if(!m.search()){
+				return names;
+			}
+			else{
+				return names.filter(function(){
+					return names.indexOf(m.search())!=-1;
+				});
+			}
+		}),
 		locations:[
 			{
 				name:'天安门',
+				ENname:'tiananmen',
 				position:[116.397391,39.908743]
 			},
 			{
 				name:'故宫博物院',
+				ENname:'Forbidden City',
 				position:[116.396919,39.917959]
 			},
 			{
 				name:'汉庭酒店',
+				ENname:'Huazhu Hotels',
 				position:[116.403227,39.911376]
 			},
 			{
 				name:'医院',
+				ENname:'Hospital',
 				position:[116.403914,39.920362]
 			},
 			{
 				name:'王府井百货',
+				ENname:'Wangfujing Department Store',
 				position:[116.409922,39.91345]
 			}
 		],
@@ -43,6 +71,9 @@ var map;
 			for(let i=0;i<m.markers.length;i++){
 				if(i===index){
 						m.markers[i].setAnimation('AMAP_ANIMATION_BOUNCE');
+						setTimeout(function(){
+								m.markers[i].setAnimation('AMAP_ANIMATION_NONE');
+						},2400);
 				}
 				else{
 					m.markers[i].setAnimation('AMAP_ANIMATION_NONE');
@@ -51,9 +82,14 @@ var map;
 		}
 	};
 
-	var v={
-		
-	};
+
+
+	/*测试*/
+	m.fn();
+	/*测试*/
+
+
+
 
 	var vm={
 		self:this,
@@ -64,36 +100,34 @@ var map;
 				resizeEnable: true,
 				zoom:11,
 				center: m.initCenter,
-				mapStyle:'amap://styles/04736ff583c7a006b6300e746254d625'  
+				mapStyle:'amap://styles/04736ff583c7a006b6300e746254d625'
 			});
 
 			//添加控件
 			AMap.plugin(['AMap.ToolBar','AMap.Scale','AMap.OverView'],function(){
 				 map.addControl(new AMap.ToolBar({position:'RT',autoPosition:false}));
-				
+
 			});
 			AMapUI.loadUI(['control/BasicControl'],function(BasicControl){
 				 map.addControl(new BasicControl.LayerSwitcher({position: 'rb'}));
 			});
-			//初始化markers 和infowindows
+			//初始化markers 
 			for(let i=0;i<m.locations.length;i++){
-				vm.setMarker(m.locations[i].position,m.locations[i].name);
-				vm.setInfowindow(m.locations[i].name);
+				vm.setMarker(m.locations[i].position,m.locations[i].name,m.locations[i].ENname);
+				vm.getWiki(m.locations[i].ENname);
+				
 			}
-
 			//初始化marker 和事件添加
 			for(let i=0;i<m.locations.length;i++){
+
 				  AMap.event.addListener(m.markers[i], 'click', function() {
-            		m.infowindows[i].open(map, m.markers[i].getPosition());
-            		//为标记添加点击动画
-            		if(m.jump){
-            			m.markers[i].setAnimation('AMAP_ANIMATION_NONE');
-            		}
-            		else{
-            			m.markers[i].setAnimation('AMAP_ANIMATION_BOUNCE');
-            		}
-            		m.jump=!m.jump;
-       			});
+			            		vm.setInfowindow(m.html[i]).open(map, m.markers[i].getPosition());
+			            		//为标记添加点击动画
+			            		m.markers[i].setAnimation('AMAP_ANIMATION_BOUNCE'); 
+			            		setTimeout(function(){
+							m.markers[i].setAnimation('AMAP_ANIMATION_NONE');
+						},2400);
+			       		});
 			}
 		},
 		initMain:function(){
@@ -101,7 +135,7 @@ var map;
 			ko.applyBindings(m);
 			vm.initMap();
 			vm.eventAdd();
-			
+
 
 		},
 		eventAdd:function(){
@@ -111,14 +145,15 @@ var map;
 			});
 
 		},
-		setMarker:function(position,title){
+		setMarker:function(position,title,ENname){
 			//TODO 设置marker并存入marker数组
 			var marker=new AMap.Marker({
 				position:position,
 				title:title,
 				map:map,
 				animation:'AMAP_ANIMATION_DROP',
-				offset:{x:-10,y:0}
+				offset:{x:-10,y:0},
+				extData:ENname
 			});
 			m.markers.push(marker);
 		},
@@ -132,7 +167,8 @@ var map;
 			var infowindow=new AMap.InfoWindow({
 				content:info
 			});
-			m.infowindows.push(infowindow);
+			// m.infowindows.push(infowindow);
+			return infowindow;
 		},
 		getInfowindow:function(index){
 			//TODO 从infowindows中取infowindow
@@ -141,23 +177,45 @@ var map;
 		panel_animation:function(){
 			//TODO 	修改transform_width 实现panel动画
 			m.panel_toggle?m.transform_width(-m.header_title_width):m.transform_width(0);
-			m.panel_toggle=!m.panel_toggle;	
+			m.panel_toggle=!m.panel_toggle;
 		},
 		getInfoFromWiki:function(location){
 			//TODO 第三方API获取地点相关数据
+		},
+		getWiki:function(location){
+			//TODO 获取wiki数据
+			var url='https://en.wikipedia.org/w/api.php?action=opensearch&search=';
+			var requestUrl=url+location+'&prop=revisions&rvprop=content&format=json';
+				$.ajax({
+					url:requestUrl,
+					dataType:'jsonp',
+					method:'get'
+				}).done(function(data){
+					var html='<h1><a href='+data[3][0]+'>'+data[0]+'</a></h1>';
+					html+='<p>'+data[2][0]+'</p>';
+					switch(location){
+						case 'tiananmen':m.html[0]=html;break;
+						case 'Forbidden City':m.html[1]=html;break;
+						case 'Huazhu Hotels':m.html[2]=html;break;
+						case 'Hospital':m.html[3]=html;break;
+						case 'Wangfujing Department Store':m.html[4]=html;break;
+					}					
+				}).fail(function(){
+					alert('数据请求出错 请刷新试一下');
+				});
 		}
 
 	};
 
 	vm.initMain();
 	map.setFitView();
+	
 
 });
 
-	
+
 
 
 
 
 }
-
